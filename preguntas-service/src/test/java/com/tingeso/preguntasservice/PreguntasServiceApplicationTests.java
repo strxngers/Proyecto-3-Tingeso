1475a6ecb3c34d455cef8de689608dd08910e72d
Elena Guzmán <91577901+strxngers@users.noreply.github.com>
2023-07-17 11:46:08 -0400

    prueba

---
+++ preguntas-service/src/test/java/com/tingeso/preguntasservice/PreguntasServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tingeso.preguntasservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreguntasServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
